f7c22bffb9403d89835a6dbf9bbc2de6afa08f00
Zakieva <rnjns78@icloud.com>
2024-10-24 11:55:25 +0300

    Update __init__.py

--- Python/my_package/app/__init__.py
+++ Python/my_package/app/__init__.py
@@ -1,10 +1,12 @@
 """lesson3."""
 
-print("RUN __INIT__")
-
 # from .modul1 import *
 # from .modul2 import *
 
 # as псевдо нейм,устранение конфликта пространста имен
 from .modul1 import value as val1
 from .modul2 import value as val2
+
+print("RUN __INIT__")
+
+
